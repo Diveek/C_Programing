abe32b7de2c19369fbbe0a27756aa71bdf39fbde
Diveek <diveek3125@gmail.com>
2024-06-03 09:56:13 +0530

    Arithmetic Operators

---
+++ Arithmetic_Operators.c
@@ -0,0 +1,16 @@
+/* The Binary Arithmetic operators are:( + , - , * , / )And Modulus operator %       */
+
+#include <stdio.h> /* program to check leap year*/
+
+int main()
+{
+    int year;
+    year=2024;
+
+    if((year%4==0 && year%100 !=0 )|| (year%400==0))
+        printf("%d is a leap year.",year);
+    else
+        printf("%d is a not leap year.",year);
+
+    return 0;
+}
